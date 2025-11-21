92c5f34e301896d27b872fc63dbcfadd95acee2f
Shikha Surana <114497416+surana01@users.noreply.github.com>
2025-11-21 17:42:10 +0000

    fix imports

--- tests/alf/core/dummy_modules/__init__.py
+++ tests/alf/core/dummy_modules/__init__.py
@@ -1,3 +1,3 @@
-from .acquisition_function import DummyAcquisitionFunction
-from .dataset import DummyDataset
-from .model import DummyModel
+from tests.alf.core.dummy_modules.acquisition_function import DummyAcquisitionFunction
+from tests.alf.core.dummy_modules.dataset import DummyDataset
+from tests.alf.core.dummy_modules.model import DummyModel
