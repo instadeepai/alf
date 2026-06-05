ec4fc401999fa82a0d40c7b273ec19dded6e7d72
olive005 <o.gallup@instadeep.com>
2026-06-05 16:14:21 +0200

    fix: linting

--- tools/alf_tools/optimizer/acquisition_functions/__init__.py
+++ tools/alf_tools/optimizer/acquisition_functions/__init__.py
@@ -12,14 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from alf_tools.optimizer.acquisition_functions.botorch_acquisition import (
-    BoTorchAcquisition,
-)
+from alf_tools.optimizer.acquisition_functions.botorch_acquisition import BoTorchAcquisition
 from alf_tools.optimizer.acquisition_functions.botorch_samplers import BoTorchMCSampler
 from alf_tools.optimizer.acquisition_functions.core_set import CoreSet
-from alf_tools.optimizer.acquisition_functions.expected_improvement import (
-    ExpectedImprovement,
-)
+from alf_tools.optimizer.acquisition_functions.expected_improvement import ExpectedImprovement
 from alf_tools.optimizer.acquisition_functions.greedy import Greedy
 from alf_tools.optimizer.acquisition_functions.thompson_sampling import ThompsonSampling
 from alf_tools.optimizer.acquisition_functions.ucb import UCB
